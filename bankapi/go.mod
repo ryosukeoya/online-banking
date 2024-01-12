4f50f4cfd11ea57fde98a7a3ab70fefc9efac483
Ryosuke Oya <engineerxx1256@gmail.com>
2024-01-12 14:45:49 +0900

    initial commit

---
+++ bankapi/go.mod
@@ -0,0 +1,7 @@
+module bankapi
+
+go 1.21.5
+
+replace github.com/msft/bank => ../bankcore
+
+require github.com/msft/bank v0.0.1 // indirect
---
+++ bankcore/bank.go
@@ -0,0 +1,53 @@
+package bank
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Customer ...
+type Customer struct {
+	Name    string
+	Address string
+	Phone   string
+}
+
+// Account ...
+type Account struct {
+	Customer
+	Number  int32
+	Balance float64
+}
+
+func Hello() string {
+	return "Hey! I'm working!"
+}
+
+// Deposit ...
+func (a *Account) Deposit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount to deposit should be greater than zero")
+	}
+
+	a.Balance += amount
+	return nil
+}
+
+// Withdraw ...
+func (a *Account) Withdraw(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount to withdraw should be greater than zero")
+	}
+
+	if a.Balance < amount {
+		return errors.New("the amount to withdraw should be greater than the account's balance")
+	}
+
+	a.Balance -= amount
+	return nil
+}
+
+// Statement ...
+func (a *Account) Statement() string {
+	return fmt.Sprintf("%v - %v - %v", a.Number, a.Name, a.Balance)
+}
